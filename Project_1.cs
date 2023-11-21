d9b88e656f59bd72722f1d1f423d76bad3cd0e1b
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-21 13:50:20 +0300

    my first project

---
+++ Project_1.cs
@@ -0,0 +1,11 @@
+﻿// Выведите на экран сообщение: «Программирование на C#!».
+
+// Вы можете вывести сообщение любым способом.
+
+using System;
+
+public class itProger {
+    public static void Main() {
+        Console.WriteLine ("Программирование на C#! ");
+    }
+}
