18d42ac35f6827227b4d4a861691e21c4029c7a1
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-21 14:22:02 +0300

    my 5 project

---
+++ Project_5.cs
@@ -0,0 +1,14 @@
+// Создайте цикл for от 1 до 5 включительно.
+//  В каждой итерации выводите на экран фразу: «Номер элемента: num», где num – это индекс каждой итерации.
+
+
+
+using System;
+
+public class itProger {
+    public static void Main() {
+      for (short i =1; i<=5; i++)
+      Console.WriteLine ("Номер элемента: "+ i);
+    
+    }
+}
